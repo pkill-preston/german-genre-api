f798bf3a205f09411c8621717fba72687a2a51dd
pkill-preston <0v3rthinker@protonmail.com>
2026-05-29 12:38:14 -0300

    adding Number() convert on port definition

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`API running on port ${PORT}`);
